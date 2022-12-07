54088ff3b1c7ff01f015d75a0ae3752185050c57
Timothy Axford <t.axford2@yahoo.com>
2022-12-06 20:16:02 -0500

    Added example of adding to parameters and calling the result on the console.

--- Methods.Net5/Program.cs
+++ Methods.Net5/Program.cs
@@ -5,7 +5,7 @@ namespace Methods.Net5
     internal class Program
     {
 
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             WriteSomething();
             WriteSomethingSpecific("I am an argument and am called from a method");
@@ -13,15 +13,29 @@ namespace Methods.Net5
 
         //method example
         // access modifer (static) return type methd name (parameter1, parameter2)
-        public static void WriteSomething()
+        /*public static void WriteSomething()
         {
             Console.WriteLine("I am called from a method");
-        }
+        }*/
 
-        public static void WriteSomethingSpecific(string myText)
+        /*public static void WriteSomethingSpecific(string myText)
         {
             Console.WriteLine(myText);
             Console.Read();
+        }*/
+
+
+        //examples of methods with retrurn value and parameters
+        static void Main(string[] args)
+        {
+            int result = Add(15, 31);
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
         }
 
         
